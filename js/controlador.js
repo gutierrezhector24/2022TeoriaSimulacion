35aa4bb588a7bae70c018c2491be1f02b34f846f
Hector Gutierrez <78827908+gutierrezhector24@users.noreply.github.com>
2022-04-16 08:48:56 -0600

    html css js casi listo

---
+++ js/controlador.js
@@ -0,0 +1,41 @@
+var Empleados = [];
+var Ordenes = [];
+
+
+function abrirOrden(){
+ document.getElementById('containerEmploye').style.display = 'none';
+ document.getElementById('containerPrincipal').style.display = 'none';
+ document.getElementById('containerOrden').style.display = 'block';
+}
+
+function abrirEmpleado(){
+    document.getElementById('containerEmploye').style.display = 'block';
+    document.getElementById('containerPrincipal').style.display = 'none';
+    document.getElementById('containerOrden').style.display = 'none';
+}
+
+function abrirIniciales(){
+    document.getElementById('containerEmploye').style.display = 'none';
+    document.getElementById('containerPrincipal').style.display = 'block';
+    document.getElementById('containerOrden').style.display = 'none';
+}
+
+function agregarEmpleado(){
+    var Empleado = {
+        nombres: document.getElementById('nombresEmpleado').value,
+        apellidos: document.getElementById('apellidosEmpleado').value,
+        fechaIngreso: document.getElementById('fechaIngreso').value      
+    }
+
+    Empleados.push(Empleado);
+    console.log(Empleados);
+}
+
+function agregarOrden(){
+    var Orden = {
+        id: document.getElementById('idOrden').value,
+        tipo: document.getElementById('tipoOrden').value
+    };
+    Ordenes.push(Orden);
+    console.log(Ordenes);
+}
